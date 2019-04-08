d4aa23272f2f2a7061eb07f2704fc832ddabbbc7
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-08 15:45:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,84 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def display_board(b)
+  puts " #{b[0]} | #{b[1]} | #{b[2]} "
+  puts "-----------"
+  puts " #{b[3]} | #{b[4]} | #{b[5]} "
+  puts "-----------"
+  puts " #{b[6]} | #{b[7]} | #{b[8]} "
+end
+
+
+def position_taken?(board, index)
+  return !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def valid_move?(board, index)
+  return (index.between?(0,8) && !position_taken?(board, index))
+end
+
+def input_to_index(user_input)
+  user_input = user_input.to_i - 1
+end
+
+
+def move(array,index,player)
+  array[index] = player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = input_to_index(gets.strip)
+  if valid_move?(board, input)
+    move(board, input)
+  else
+    turn(board)
+  end
+  display_board(board)
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |square|
+    if square == "X" || square == "O"
+      counter += 1
+    else end
+    end
+    return counter
+  end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |condition|
+    if [board[condition[0]],board[condition[1]], board[condition[2]]].all?("X") || [board[condition[0]],board[condition[1]], board[condition[2]]].all?("O")
+      return condition
+      end
+    end
+    return false
+end
+
+
+def full?(board)
+  !board.any?(" " || nil)
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+  won?(board) ? board[won?(board)[0]] : nil
+end
